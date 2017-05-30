b186538a78e4a57a8c9a58b811cd44e9e5948437
Сергей Владимирович Лавров <bizy@Air-Sergej.Dlink>
2017-05-30 14:37:16 +0300

    added complete template and view

--- project/vkauth/views.py
+++ project/vkauth/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from social_django.utils import psa
+
+
+@psa('social:complete')
+def register_by_access_token(request, backend):
+    token = request.GET.get('access_token')
+
+    return render('complete.html', context={'token': token})
