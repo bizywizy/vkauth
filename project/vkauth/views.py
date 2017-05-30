28fba6c51d83c7baff331e5a62dba8b498352bb6
Сергей Владимирович Лавров <bizy@Air-Sergej.Dlink>
2017-05-30 16:46:58 +0300

    trying

--- project/vkauth/views.py
+++ project/vkauth/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
+import vk
 
 @login_required
 def user(request):
-    user = User.objects.get(pk=request.user.pk)
-    social = user.social_auth.get(provider='vk-oauth2')
+    social = request.user.social_auth.get(provider='vk-oauth2')
     token = social.extra_data.get('access_token')
-    return render(request, 'complete.html', context={'token': token})
+    vkapi = vk.API(access_token=token)
+    friends = vkapi.friends.get(order='random', count=5)
+    return render(request, 'complete.html', context={'friends': friends})
