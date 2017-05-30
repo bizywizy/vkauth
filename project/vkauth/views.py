d42cacd8a735575f2bcb00d629117010e4057ced
Сергей Владимирович Лавров <bizy@Air-Sergej.Dlink>
2017-05-30 16:16:12 +0300

    trying

--- project/vkauth/views.py
+++ project/vkauth/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from django.contrib.auth.models import User
 
 @login_required
 def user(request):
-    return render(request, 'complete.html', context={'items': dir(request.user)})
+    user = User.objects.get(pk=request.user.pk)
+    social = user.social_auth.get(provider='VKOAuth2')
+    return render(request, 'complete.html', context={'social': social})
