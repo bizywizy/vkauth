8065ec99fe80ae1539f78e5a05b90752043559fd
Сергей Владимирович Лавров <bizy@Air-Sergej.Dlink>
2017-05-30 17:14:16 +0300

    trying

--- project/vkauth/urls.py
+++ project/vkauth/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls import url
 
-from django.views.generic import TemplateView
-
-from .views import user
+from .views import index_view
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='index.html'), name='login'),
-    url(r'user/$', user, name='user')
+    url(r'^$', index_view, name='index')
 ]
--- project/vkauth/views.py
+++ project/vkauth/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 import vk
 
 
-@login_required
-def user(request):
-    social = request.user.social_auth.get(provider='vk-oauth2')
-    token = social.extra_data.get('access_token')
-    session = vk.Session(access_token=token)
-    vkapi = vk.API(session)
-    friends = vkapi.friends.get(order='random', count=5, fields=['domain', 'first_name', 'last_name'])
-    return render(request, 'complete.html', context={'friends': friends})
+def index_view(request):
+    context = {}
+    if request.user.is_authenticated:
+        social = request.user.social_auth.get(provider='vk-oauth2')
+        token = social.extra_data.get('access_token')
+        session = vk.Session(access_token=token)
+        vkapi = vk.API(session)
+        friends = vkapi.friends.get(order='random', count=5,
+                                    fields=['domain', 'first_name', 'last_name'])
+        context.update(friends)
+    return render(request, 'index.html', context=context)
